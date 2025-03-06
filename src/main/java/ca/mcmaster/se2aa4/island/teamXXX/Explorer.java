eff9bbbbeb17985c408ef1ec73c4c71f7546d93e
hoangm5 <hoangm5@mcmaster.ca>
2025-03-05 20:18:40 -0500

    Initial setup: Created Explorer class with basic structure

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Explorer.java
@@ -1,51 +1,39 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
-import java.io.StringReader;
+import eu.ace_design.island.bot.IExplorerRaid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import java.io.StringReader;
 
 public class Explorer implements IExplorerRaid {
-
     private final Logger logger = LogManager.getLogger();
+    private int battery;
+    private String heading;
 
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Initialization info:\n {}",info.toString(2));
-        String direction = info.getString("heading");
-        Integer batteryLevel = info.getInt("budget");
-        logger.info("The drone is facing {}", direction);
-        logger.info("Battery level is {}", batteryLevel);
+        this.heading = info.getString("heading");
+        this.battery = info.getInt("budget");
+        logger.info("Battery level: {}", battery);
+        logger.info("Drone is facing: {}", heading);
     }
 
     @Override
     public String takeDecision() {
-        JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
-        logger.info("** Decision: {}",decision.toString());
-        return decision.toString();
+        return new JSONObject().put("action", "stop").toString();
     }
 
     @Override
     public void acknowledgeResults(String s) {
-        JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Response received:\n"+response.toString(2));
-        Integer cost = response.getInt("cost");
-        logger.info("The cost of the action was {}", cost);
-        String status = response.getString("status");
-        logger.info("The status of the drone is {}", status);
-        JSONObject extraInfo = response.getJSONObject("extras");
-        logger.info("Additional information received: {}", extraInfo);
+        logger.info("** Results received: {}", s);
     }
 
     @Override
     public String deliverFinalReport() {
-        return "no creek found";
+        return "Mission Stopped.";
     }
-
 }
